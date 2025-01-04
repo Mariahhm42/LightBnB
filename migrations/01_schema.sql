66d82017b675b151e5ff1d742cbaecdbcc8b5bee
Mariam Agboola <mariahhm42@gmail.com>
2025-01-04 05:06:00 -0700

    created a seed file and some temporary data

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -21,14 +21,14 @@ CREATE TABLE properties (
   parking_spaces INTEGER NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
-  thumbnail_photo_url TEXT NOT NULL,
-  cover_photo_url TEXT NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(10) NOT NULL,
-  is_active BOOLEAN DEFAULT TRUE,
+  active BOOLEAN DEFAULT TRUE,
   owner_id INT REFERENCES users(id) ON DELETE CASCADE
 );
 
@@ -43,10 +43,10 @@ CREATE TABLE reservations (
 
 -- Create Property Reviews Table
 CREATE TABLE property_reviews (
-  id SERIAL PRIMARY KEY,
-  message TEXT,
-  rating INT CHECK (rating BETWEEN 1 AND 5) DEFAULT 0,
-  guest_id INT REFERENCES users(id) ON DELETE CASCADE,
-  reservation_id INT REFERENCES reservations(id) ON DELETE CASCADE,
-  property_id INT REFERENCES properties(id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    message TEXT,
+    rating SMALLINT CHECK (rating BETWEEN 0 AND 5) DEFAULT 0,
+    guest_id INT REFERENCES users(id) ON DELETE CASCADE,
+    reservation_id INT REFERENCES reservations(id) ON DELETE CASCADE,
+    property_id INT REFERENCES properties(id) ON DELETE CASCADE
 );
